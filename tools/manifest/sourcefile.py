38528cab4072cbe2581ee627f96ff1b2e29e4b70
Simon Pieters <zcorpan@gmail.com>
2018-05-29 16:52:26 +0200

    Change to NOTIFY

--- tools/manifest/sourcefile.py
+++ tools/manifest/sourcefile.py
@@ -254,7 +254,7 @@ class SourceFile(object):
                 self.name_prefix("MANIFEST") or
                 self.filename.startswith(".") or
                 self.filename == "OWNERS" or
-                self.filename == "REVIEWERS" or
+                self.filename == "NOTIFY" or
                 self.type_flag == "support" or
                 self.in_non_test_dir())
 
